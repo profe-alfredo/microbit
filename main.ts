33b5312241d02226d0bf47322058ea7f7fa0efb2
Prof. Alfredo <alfredo.profe10@gmail.com>
2025-12-01 14:16:26 -0600

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,13 @@
 input.onButtonPressed(Button.A, function () {
     basic.showString("" + (input.temperature()))
+    basic.pause(1000)
 })
 input.onButtonPressed(Button.B, function () {
     basic.showString("" + (input.compassHeading()))
+    basic.pause(1000)
 })
 basic.showString("Sensors")
+basic.showString("A: Temp, B: Compas")
 basic.forever(function () {
-    basic.showString("A: Temp, B: Compás")
+	
 })
