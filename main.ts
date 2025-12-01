2d35bd6e6bf704083777563f0a2ecb4e423f4db6
Prof. Alfredo <alfredo.profe10@gmail.com>
2025-12-01 13:10:10 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,10 @@
-
+input.onButtonPressed(Button.A, function () {
+    basic.showString("" + (input.temperature()))
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("" + (input.compassHeading()))
+})
+basic.showString("Sensors")
+basic.forever(function () {
+    basic.showString("A: Temp, B: Compás")
+})
