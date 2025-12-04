0dfc0fa770e6ca173c425a084ea4bcd7968a34f1
Prof. Alfredo <alfredo.profe10@gmail.com>
2025-12-04 00:48:10 -0600

    Update 7-seconds/pxt.json, 7-seconds/main.blocks, 7-seconds/main.ts

---
+++ 7-seconds/main.ts
@@ -0,0 +1,8 @@
+bluetooth.onBluetoothConnected(function () {
+    basic.showIcon(IconNames.Heart)
+})
+bluetooth.onBluetoothDisconnected(function () {
+    basic.showIcon(IconNames.No)
+})
+bluetooth.startUartService()
+basic.showIcon(IconNames.No)
